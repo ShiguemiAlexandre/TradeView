6273d08399caf42b769ec7e5b47ebe901a2077bd
Alexandre Ito <shiguemialexandre@gmail.com>
2024-11-07 23:39:40 -0300

    BK

--- tradeview/pages/home/home.py
+++ tradeview/pages/home/home.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import yfinance as yf
+from datetime import datetime, timedelta
 
-msft = yf.Ticker("^BCFF11")
+lista_ativos = []
+# msft = yf.Ticker(["AAPL", "BRK.B", "GOOGL", "INTC", "MSFT"])
+ativo = "AAPL"
+msft = yf.Ticker(ativo)
+
 st.write(msft.info)
